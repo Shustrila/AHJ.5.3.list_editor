3aef979a3f1887799be2a9ba940783f66f0dfaf5
Alexandr Maxalov <sanya.maxalov@gmail.com>
2019-05-20 00:24:56 +0300

    e2e tests

--- e2e/ListEditor.spec.js
+++ e2e/ListEditor.spec.js
@@ -1,11 +1,12 @@
 import puppetteer from 'puppeteer';
 
-jest.setTimeout(2000000);
+jest.setTimeout(20000);
 
 describe('TESTS: list editor', () => {
     let browser = null;
     let page = null;
-    const baseUrl = 'http://localhost:8080';
+    const baseUrl = 'https://shustrila.github.io/AHJ.5.3.list_editor/';
+
     beforeAll(async () => {
         browser = await puppetteer.launch({
             headless: false,
@@ -14,53 +15,64 @@ describe('TESTS: list editor', () => {
         });
         page = await browser.newPage();
     });
-    afterAll(async () => {
-        await browser.close();
-    });
-
 
     test('1. no validate fields', async () => {
         await page.goto(baseUrl);
-        const add = await page.$('[data-editor=add]');
-        await add.click();
-        const ok = await page.$('.form .form__button-ok');
-        await ok.click();
-        const name = await page.$(".form [name=name]");
+        await page.click('[data-editor=add]');
+        await page.click('.form .form__button-ok');
 
-        console.log(name.parent('.form__label'));
-
-        await name.parentNode.waitForSelector('.form__error');
-        const price = await page.$(".form [name=price]");
-        await price.parentNode.waitForSelector('.form__error');
+        await page.$(".form form__label", async item => {
+            await item.waitForSelector('.form__error');
+        });
     });
 
-    // test('2. add row', async () => {
-    //     await page.goto(baseUrl);
-    //     const add = await page.$('[data-editor=add]');
-    //     await add.click();
-    //     const name = await page.$('.form [name=name]');
-    //     await name.type('iPhone X');
-    //     const price = await page.$('.form [name=price]');
-    //     await price.type('60000');
-    //     const ok = await page.$('.form .form__button-ok');
-    //     await ok.click();
-    //     await page.waitForSelector('[data-goods-id="0"]');
-    // });
-
-    test('3. remove row id 0', async () => {
+    test('2. add row', async () => {
         await page.goto(baseUrl);
-        const add = await page.$('[data-editor=add]');
-        await add.click();
+        await page.click('[data-editor=add]');
         const name = await page.$('.form [name=name]');
         await name.type('iPhone X');
         const price = await page.$('.form [name=price]');
         await price.type('60000');
-        const ok = await page.$('.form .form__button-ok');
-        await ok.click();
-        const row = await page.$('[data-goods-id="0"]');
-        const remove = await row.$('.table__remove');
-        await remove.click();
-        let dialogExists = false;
-        await  page.on('dialog', () => dialogExists = true)
-    })
+        await page.click('.form .form__button-ok');
+        await page.waitForSelector('[data-goods-id="0"]');
+    });
+
+    test('3. remove row id 0', async () => {
+        await page.goto(baseUrl);
+        await page.click('[data-editor=add]');
+        const name = await page.$('.form [name=name]');
+        await name.type('iPhone X');
+        const price = await page.$('.form [name=price]');
+        await price.type('60000');
+        await page.click('.form .form__button-ok');
+        page.on('dialog', async dialog => {
+            await dialog.accept();
+        });
+        await page.click('[data-goods-id="0"] .table__remove');
+
+    });
+
+    test('4. edit row', async () => {
+        await page.goto(baseUrl);
+        await page.click('[data-editor=add]');
+        const name = await page.$('.form [name=name]');
+        await name.type('iPhone X');
+        const price = await page.$('.form [name=price]');
+        await price.type('9000');
+        await page.click('.form .form__button-ok');
+        await page.click('[data-goods-id="0"] .table__edit');
+        const priceEdit = await page.$('.form [name=price]');
+        await priceEdit.type('0');
+        await page.click('.form .form__button-ok');
+
+        const result = await page.evaluate(() => {
+            return document.querySelector('[data-goods-id="0"] [data-editor-row=price]').innerText;
+        });
+
+        expect(result).toBe('90000');
+    });
+
+    afterAll(async () => {
+        await browser.close();
+    });
 });
